3c6245e4afa5f012d351806479d4705030dcd437
Cloudsty <wi13b051@gmail.com>
2025-09-11 13:02:19 +0100

    fix: add cors fix

--- Src/APIGateway.Proxy/Program.cs
+++ Src/APIGateway.Proxy/Program.cs
@@ -44,7 +44,7 @@ try
     app.UseHttpLogging();
     app.MapHealthChecks("/healthz");
     app.MapHealthChecks("/ready");
-    app.UseCors();
+    app.UseCors(Cors.TranzrAPIGatewayCorsPolicy);
     app.UseAuthentication();
     app.UseAuthorization();
     app.MapReverseProxy().RequireAuthorization();
