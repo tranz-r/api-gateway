454bb45d4cc124baef962b87cbb279edc4d5d85d
Vavy <mcvavy@gmail.com>
2026-09-16 09:42:13 +0100

    fix(logging): exclude gateway query values (#2)
    Co-authored-by: Cloudsty <wi13b051@gmail.com>

---
+++ Src/APIGateway.Proxy/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("APIGateway.Proxy.UnitTests")]
---
+++ Tests/APIGateway.Proxy.UnitTests/Configuration/HttpLoggingConfigurationTests.cs
@@ -0,0 +1,28 @@
+﻿using APIGateway.Proxy.Configuration;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace APIGateway.Proxy.UnitTests.Configuration;
+
+public sealed class HttpLoggingConfigurationTests
+{
+    [Fact]
+    public void AddTranzrHttpLogging_ExcludesSensitiveQueryValues()
+    {
+        var services = new ServiceCollection();
+
+        services.AddTranzrHttpLogging();
+
+        using var provider = services.BuildServiceProvider();
+        var options = provider.GetRequiredService<IOptions<HttpLoggingOptions>>().Value;
+
+        var expectedFields = HttpLoggingFields.RequestMethod
+            | HttpLoggingFields.RequestPath
+            | HttpLoggingFields.ResponseStatusCode
+            | HttpLoggingFields.Duration;
+
+        Assert.Equal(expectedFields, options.LoggingFields);
+    }
+}
